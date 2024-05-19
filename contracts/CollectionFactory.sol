b4adf148c8c55ad001645e90e6e7cb90d7888148
tal waserman <talwaserman@gmail.com>
2024-05-19 15:27:18 +0300

    add collection name to event

--- contracts/CollectionFactory.sol
+++ contracts/CollectionFactory.sol
@@ -11,16 +11,17 @@ contract CollectionFactory {
         owner = msg.sender;
     }
 
-    event CollectionDeployedLog(address indexed collectionAddress, address indexed owner);
+    event CollectionDeployedLog(address indexed collectionAddress, address indexed owner, string collectionName);
 
     function deployCollection(string memory collectionName,
         uint256[] memory ids,
         uint256[] memory totalAmounts,
         string memory baseHash,
-        address marketplaceAccount) external returns(address){
+        address marketplaceAccount) external returns(address) {
+        // TODO: add checks for inputs
         Collection newCollection = new Collection(collectionName, ids, totalAmounts, baseHash, marketplaceAccount);
         collections[address(newCollection)] = msg.sender;
-        emit CollectionDeployedLog(address(newCollection), msg.sender);
+        emit CollectionDeployedLog(address(newCollection), msg.sender, collectionName);
         return address(newCollection);
     }
 }
